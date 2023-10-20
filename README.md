210be9232ff3e37a7e53635c8c2b0a30e3f60728
Mykyta <mykyta.kovalenko@proarea.com>
2023-10-20 12:59:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_web_portfolio
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_portfolio/app.dart';
+
+void main() {
+  runApp(const MainApp());
+}
